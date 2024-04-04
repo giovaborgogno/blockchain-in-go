6742e27b87c87b5cc1abf38c734b42eb83e0c771
Giovanni Borgogno <giovaborgogno@gmail.com>
2024-04-04 10:49:29 -0300

    first commit

---
+++ Makefile
@@ -0,0 +1,8 @@
+build:
+	go build -o ./bin/blockchain-in-go
+
+run: build
+	./bin/blockchain-in-go
+
+test:
+	go test -v ./...
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"time"
+
+	"github.com/giovaborgogno/blockchain-in-go/network"
+)
+
+// Server
+// Transport => tcp, udp,
+// Block
+// Tx
+// Keypairs
+func main() {
+	trLocal := network.NewLocalTransport("LOCAL")
+	trRemote := network.NewLocalTransport("REMOTE")
+
+	trLocal.Connect(trRemote)
+	trRemote.Connect(trLocal)
+
+	go func() {
+		for {
+			trRemote.SendMessage(trLocal.Addr(), []byte("hello world"))
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	opts := network.ServerOpts{
+		Transports: []network.Transport{trLocal},
+	}
+
+	s := network.NewServer(opts)
+	s.Start()
+}
